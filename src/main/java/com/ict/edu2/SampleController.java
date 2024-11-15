a7f16249ea9e88d1197ad1b221e1ee8864db12fa
munminwon <minwon9276@naver,com>
2024-11-15 15:48:55 +0900

    3번째 변경

--- src/main/java/com/ict/edu2/SampleController.java
+++ src/main/java/com/ict/edu2/SampleController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -93,7 +95,57 @@ public class SampleController {
           return "hi, 안녕 ";
       }
       @PostMapping("/hi2")
+      // 일반적인 파라미터는 get/post가 같다
       public String[] getHi2(String idx) {
           return new String[] {"Hello","World "};
       }
+
+      @PostMapping("/hi3")
+      // 일반적인 파라미터는 get/post가 같다
+      public String[] getHi3(@RequestParam("idx") String idx) {
+          return new String[] {"Hello","World "};
+      }
+
+      @PostMapping("/hi4")
+      // 요청자체가 json 방식일때
+      public String[] getHi4(@RequestBody Map<String,String> idx) {
+          return new String[] {"Hello","World "};
+      }
+
+// 반환형이 객체  일 때
+    @PostMapping("/hi5")
+        public SampleVO getHi5() {
+        SampleVO svo =new SampleVO();
+        svo.setName("고길동");
+        svo.setAge("24");
+
+        return svo;
+        }
+        @PostMapping("/hi7")
+        public DataVO getHi7(String number) {
+            DataVO dataVO = new DataVO();
+            
+            List<SampleVO> list = new ArrayList<>();
+  
+            SampleVO svo =new SampleVO("고길동 " , "24");
+            SampleVO svo2 =new SampleVO("홍길동" , "100");
+            SampleVO svo3 =new SampleVO("희동이","3");    
+            
+            list.add(svo);
+            list.add(svo2);
+            list.add(svo3);
+            
+            if(number.equals("1")) {
+              // dataVO.setResultCode(1);
+              // dataVO.setMessage("성공");
+              // dataVO.setData(list);
+  
+              return DataVO.success(list, "성공");
+            }else{
+              // dataVO.setResultCode(0);
+              // dataVO.setMessage("실패");
+              // dataVO.setData(null);
+              return DataVO.fail(0, "실패");
+          }
+        }
     }
